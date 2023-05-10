76f4e9f0d6b12b2423c43f0b0bcc7cca946580db
johnny <jmalulan7@gmail.com>
2023-05-10 20:58:17 +0800

    Add register page, header component and links

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,14 @@ import { Dashboard } from "./pages/Dashboard";
 import { Login } from "./pages/Login";
 import { Register } from "./pages/Register";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Header } from "./components/Header";
 
 function App() {
   return (
     <>
       <Router>
         <div className="container">
+          <Header />
           <Routes>
             <Route path="/" element={<Dashboard />}></Route>
             <Route path="/login" element={<Login />}></Route>
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,3 +1,90 @@
+import { useState, useEffect } from "react";
+import { FaUser } from "react-icons/fa";
+
 export const Register = () => {
-    return(<div>Register Page</div>)
-}
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    password2: "",
+  });
+  //Destructure
+  const { name, email, password, password2 } = formData;
+
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const onSubmit = (e) => {
+    console.log("Hello world!");
+    e.preventDefault();
+  };
+
+  return (
+    <>
+      <section className="heading">
+        <h1>
+          <FaUser />
+          Register
+        </h1>
+        <p>Please create an account</p>
+      </section>
+      <section className="form">
+        <form onSubmit={onSubmit}>
+          <div className="form-group">
+            <input
+              type="text"
+              className="form-control"
+              id="name"
+              name="name"
+              value={name}
+              placeholder="Enter your name"
+              onChange={onChange}
+            ></input>
+          </div>
+          <div className="form-group">
+            <input
+              type="email"
+              className="form-control"
+              id="email"
+              name="email"
+              value={email}
+              placeholder="Enter your email"
+              onChange={onChange}
+            ></input>
+          </div>
+          <div className="form-group">
+            <input
+              type="password"
+              className="form-control"
+              id="password"
+              name="password"
+              value={password}
+              placeholder="Enter your password"
+              onChange={onChange}
+            ></input>
+          </div>
+          <div className="form-group">
+            <input
+              type="password"
+              className="form-control"
+              id="password2"
+              name="password2"
+              value={password2}
+              placeholder="Confirm your password"
+              onChange={onChange}
+            ></input>
+          </div>
+          <div className="form-group">
+            <button type="submit" class="btn btn-block">
+              Submit
+            </button>
+          </div>
+        </form>
+      </section>
+    </>
+  );
+};
